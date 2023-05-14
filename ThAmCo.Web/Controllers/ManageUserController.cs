4fc1e9034647fd84c6545eed0426e82185227245
Ciaran May-Townsend <83427326+varine-uni@users.noreply.github.com>
2023-05-14 22:56:51 +0100

    Create manager controller

---
+++ ThAmCo.Web/Controllers/ManageUserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ThAmCo.Web.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        public IActionResult ManageUser()
+        {
+            return View();
+        }
+    }
+}
